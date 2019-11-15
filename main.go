ed296e16d30977c4beb94ab3b55f5cacaa466dad
crojas <c.rojas.latorre@gmail.com>
2019-11-15 00:58:35 -0300

    bad

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"net/http"
 	"os"
@@ -30,19 +33,54 @@ type Cliente struct {
 	Degree           string `json:"degree"`
 }
 
-type ErrMsg struct {
+type RespMsg struct {
 	Message string `json:"message"`
 }
 
 var ClientList = []Cliente{}
+var SecretKey = "superawesomesecketkey"
 
 func main() {
 	router := gin.Default()
-	router.GET("/clients", GetClients)
-	router.GET("/clients/:id", GetClientByID)
-	router.POST("/clients", PostClient)
+	auth := router.Group("")
+	auth.Use(CheckSignature())
+	{
+		auth.GET("/clients", GetClients)
+		auth.GET("/clients/:id", GetClientByID)
+		auth.POST("/clients", PostClient)
+	}
+	router.POST("/signature", SignPayload)
 	router.Run(Port())
+}
 
+func respondWithError(c *gin.Context, code int, message interface{}) {
+	c.AbortWithStatusJSON(code, gin.H{"error": message})
+}
+
+func CheckSignature() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		hsign := c.Request.Header.Get("X-CB-Signature")
+		if hsign == "" {
+			respondWithError(c, http.StatusUnauthorized, "X-CB-Signature required")
+			return
+		}
+
+		// Obtiene el payload del request
+		buf := make([]byte, 1024)
+		num, _ := c.Request.Body.Read(buf)
+		payload := string(buf[0:num])
+
+		// Verifica si el payload es igual a la firma
+		h := hmac.New(sha256.New, []byte(SecretKey))
+		h.Write([]byte(payload))
+		calculated := h.Sum(nil)
+		decoded, _ := hex.DecodeString(hsign)
+		if hmac.Equal(calculated, decoded) == false {
+			respondWithError(c, http.StatusUnauthorized, "Invalid X-CB-Signature")
+			return
+		}
+		c.Next()
+	}
 }
 
 func GetClientByID(c *gin.Context) {
@@ -53,7 +91,7 @@ func GetClientByID(c *gin.Context) {
 			return
 		}
 	}
-	msg := ErrMsg{
+	msg := RespMsg{
 		Message: fmt.Sprintf("client not found with id: %v", clientID),
 	}
 	c.JSON(http.StatusNotFound, msg)
@@ -67,7 +105,8 @@ func PostClient(c *gin.Context) {
 	cli := Cliente{}
 	err := c.BindJSON(&cli)
 	if err != nil {
-		msg := ErrMsg{
+		fmt.Println(err)
+		msg := RespMsg{
 			Message: "Invalid JSON object",
 		}
 		c.JSON(http.StatusBadRequest, msg)
@@ -77,6 +116,27 @@ func PostClient(c *gin.Context) {
 		ClientList = append(ClientList, cli)
 		c.JSON(http.StatusCreated, cli)
 	}
+}
+
+func SignPayload(c *gin.Context) {
+	// Obtine el payload del request
+	// Obtiene el payload del request
+	buf := make([]byte, 1024)
+	num, _ := c.Request.Body.Read(buf)
+	payload := string(buf[0:num])
+
+	// Crea un hmac pasandole la llave privada
+	h := hmac.New(sha256.New, []byte(SecretKey))
+
+	// le agrega el json al hmac
+	h.Write([]byte(payload))
+
+	// obtiene el resultado y lo encodea como hexadecimal
+	sha := hex.EncodeToString(h.Sum(nil))
+	msg := RespMsg{
+		Message: fmt.Sprintf("Payload signature with hmac is: %v", sha),
+	}
+	c.JSON(http.StatusCreated, msg)
 }
 
 func Port() string {
